7d9c4709768115d8ce8ab15a6dfd6edf7eb3c121
kaerte <lucas.wski@gmail.com>
2021-06-14 00:14:56 +0200

    Load Laravel migrations during tests to create user table

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -13,16 +13,11 @@ abstract class TestCase extends \Orchestra\Testbench\TestCase
     {
         parent::setUp();
         $this->loadMigrationsFrom(__DIR__.'../database/migrations');
+        $this->loadLaravelMigrations();
     }
 
     protected function getPackageProviders($app): array
     {
         return [RestaurantMenuServiceProvider::class];
-    }
-
-    protected function getEnvironmentSetUp($app): void
-    {
-        include_once __DIR__.'/migrations/2014_10_12_000000_create_users_table.php';
-        (new CreateUsersTable())->up();
     }
 }
